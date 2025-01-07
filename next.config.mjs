043de9df4846efda02b69616b5d665e02cfb9386
Niba farooq <164494729+Niba-khan@users.noreply.github.com>
2025-01-07 18:58:28 +0500

    SanityDynamicBlogs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:['assets.traveltriangle.com', 'theworldtravelguy.com']
+        remotePatterns:[
+            {
+                protocol:'https',
+                hostname:'cdn.sanity.io'
+            }
+        ]
     }
 };
 
